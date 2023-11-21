839d24d2cc3241da704a6757e339da401e6145d7
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-11-20 19:13:38 -0500

    primer commit

---
+++ src/main/java/com/uce/edu/UsoGithubApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsoGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsoGithubApplication.class, args);
+	}
+
+}
