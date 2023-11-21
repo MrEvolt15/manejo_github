4512ff1626014be86372cb3ef8c4363feb9ebf30
MrEvolt15 <joelespinosa1a@hotmail.com>
2023-11-20 22:23:45 -0500

    cambio en la misma rama

--- src/main/java/com/uce/edu/UsoGithubApplication.java
+++ src/main/java/com/uce/edu/UsoGithubApplication.java
@@ -1,13 +1,19 @@
 package com.uce.edu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsoGithubApplication {
+public class UsoGithubApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsoGithubApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("primer cambio en la misma rama");
 	}
 
 }
